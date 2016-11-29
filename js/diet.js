742fcb09dd316f2b8c1456f57b7d467ce4cd639d
joshzes <joshzerica@gmail.com>
2016-11-30 01:29:29 +0800

    Fixed UI

--- js/diet.js
+++ js/diet.js
@@ -24,29 +24,23 @@ function readData(data){
 							+ "<div class='collapsible-header'>"
 								+ "<input type='checkbox' class='filled-in' id='" + tempId + "'' />"
      							+ "<label for='" + tempId + "'>" + temp + "</label>"
+					 		+ "</div>"
+							+ "<div class='collapsible-body'>"
+								+ "<table><tbody>"
+								+ "<tr><td>Price</td> <td>" + data['price'] + "</td></tr>"
+								+ "<tr><td>Serving Size</td> <td>" + data['serving'] + "</td></tr>"
+								+ "<tr><td>Calories</td> <td>" + data['calories'] + "</td></tr>"
+								+ "<tr><td>Cholesterol</td> <td>" + data['cholesterol'] + "</td></tr>"
+								+ "<tr><td>Total Fat</td> <td>" + data['total_fat'] + "</td></tr>"
+								+ "<tr><td>Sodium</td> <td>" + data['sodium'] + "</td></tr>"
+								+ "<tr><td>Carbohydrates</td> <td>" + data['carbs'] + "</td></tr>"
+								+ "<tr><td>Fiber</td> <td>" + data['fiber'] + "</td></tr>"
+								+ "<tr><td>Protein</td> <td>" + data['protein'] + "</td></tr>"
+								+ "<tr><td>Vitamin A</td> <td>" + data['vit_a'] + "</td></tr> "
+								+ "<tr><td>Vitamin C</td> <td>" + data['vit_c'] + "</td></tr> "
+								+ "<tr><td>Calcium</td> <td>" + data['calcium'] + "</td></tr> "
+								+ "<tr><td>Iron</td> <td>" + data['iron'] + "</td></tr>"
+								+ "</tbody></table>"
 							+ "</div>"
-							+ "<div class='collapsible-body'><p>"
-								+ data['price'] + ","
-								+ data['serving'] + ","
-								+ data['calories'] + ","
-								+ data['cholesterol'] + ","
-								+ data['total_fat'] + ","
-								+ data['sodium'] + ","
-								+ data['carbs'] + ","
-								+ data['fiber'] + ","
-								+ data['protein'] + ","
-								+ data['vit_a'] + ","
-								+ data['vit_c'] + ","
-								+ data['calcium'] + ","
-								+ data['iron']
-							+ "</p></div>"
 						+ "</li>"
 }
-
-/*
-
-FIX:
-	UI of collapsible-body
-	UI of button
-
-*/

cc317acdffa999962ad90b57fb7cfc303b16f3ca
joshzes <joshzerica@gmail.com>
2016-11-30 20:05:52 +0800

    Simplex optimizer for maximization added

--- js/index.js
+++ js/index.js
@@ -1,16 +1,22 @@
 $("#submitSimplex").click(function(){
+/*
 	var optMode = $('#opt_mode').prop('checked')? 1 : 0;
 	var fxString = $('#inputFx').val();
-	var constraintsList = []
+	var constraintsList = [];
 
 	var numConstraints = parseInt($(".constraint:last").attr('id').replace( /^\D+/g, ''));
 
 	for(var i = 1; i <= numConstraints;i++){
 		constraintsList[i] = $('#constraint-' + i).val();
 	}
+*/
 
-
+	var optMode = 1;
+	var fxString = "Z = 150r + 175p"
+	var constraintsList = ["7r + 11p <= 77", "10r + 8p <= 80", "r <= 9", "p <= 6"];
 	simplex(optMode, fxString, constraintsList);
+
+	showTableau(mat);
 
 	$('#simplex-container').slideUp(400);
 	$('#tableau-container').removeClass("animated slideOutDown hidden").addClass("visible");
@@ -19,6 +25,8 @@ $("#submitSimplex").click(function(){
 $("#editSimplex").click(function(){
 	$('#tableau-container').addClass("animated slideOutDown");
 	$('#simplex-container').slideDown(800);
+	$('#tableau').html("");
+
 });
 
 $("#addConstraint").click(function(){
@@ -27,3 +35,45 @@ $("#addConstraint").click(function(){
 	+ num + "'>";
 	$(nc).appendTo($("#constraints-list"));
 });
+
+$("#next-tableau").click(function(){
+	var curr = parseInt($('#current-tableau').html());
+	if(curr < tableaus.length - 1){
+		$('#tableau').html("");
+		showTableau(tableaus[curr + 1]);
+		$('#current-tableau').html(curr+1);
+	}
+});
+
+$("#prev-tableau").click(function(){
+	var curr = parseInt($('#current-tableau').html());
+	if(curr){
+		$('#tableau').html("");
+		showTableau(tableaus[curr-1]);
+		$('#current-tableau').html(curr-1);
+	}
+});
+
+function showTableau(mat){
+	var content = "";
+	var row;
+
+	content += "<thead><tr>"
+	for(var j = 0; j < (mat[0]).length; j++){
+			content += "<td>" + mat[0][j] + "</td>"
+	}
+	content += "</tr></thead>"
+
+	content += "<tbody>"
+	for(var i = 1; i < mat.length; i++){
+		content += "<tr>"
+		row = mat[i];
+		for(var j = 0; j < row.length; j++){
+			content += "<td>" + parseFloat((row[j]).toFixed(4)) + "</td>"
+		}
+		content += "</tr>"
+	}
+	content += "</tbody>"
+
+	$(content).appendTo($("#tableau"));
+}
--- js/simplex.js
+++ js/simplex.js
@@ -1,14 +1,157 @@
-var mode = 1 // 0 for minimize
-var mat = [[]];
+var mode 	// 1 for maximize, 0 for minimize
+var mat = [];	// will contain initial tableau
+var tableaus = [];
+var vars = [];
 
 function simplex(optMode, fxString, constraintsList){
 	mode = optMode;
+	mat = []
+	vars = fxString.replace(/[^a-z]/g, "").split("");
 
-	// read constraint[i] until a, b, c, d (if not found before + or -) --> 0
-	// add slack (S(i)) = 1
-	// add row to mat
+	var tableau_var = [];
 
-	// Z = 1a + 2b + 1c − 1d 
-	// read fxString until a, b, c, d (if not found before + or -) --> 0
-	// add row to mat
+	for(var i = 0; i < vars.length; i++){
+		tableau_var[i] = vars[i];
+	}
+
+	for(var i = 0; i < constraintsList.length; i++){
+		tableau_var.push("s" + (i+1));
+	}
+
+	tableau_var.push("Z", "RHS");
+
+	mat[0] = tableau_var;
+
+	 constraintsList.forEach(function(part, index, arr){
+	 	tableau_var = [];
+		var str = (arr[index]).replace(/[^a-z0-9\+\-\.\=]/g, "");
+
+		vars.forEach(function(data){
+			var pos = str.indexOf(data);
+			var coeff = 0;
+			
+			if(pos != -1){
+				for(var i = pos; i >= 0; i--){
+					if(str[i] == '+'){
+						coeff = parseFloat(str.substr(i+1, pos-1));
+						coeff = coeff ? coeff : 1;
+						break;
+		  			}
+		  			else if(str[i] == '-'){
+		  				coeff = 0 - parseFloat(str.substr(i+1, pos-1));
+		  				coeff = coeff ? coeff : -1;
+		  				break;
+		  			}
+		  			else if(i == 0){
+						coeff = parseFloat(str.substr(i, pos));
+						coeff = coeff ? coeff : 1;
+						break;
+		  			}
+				}
+			}
+
+			tableau_var.push(coeff);
+		});
+		
+		for(var i = 0; i < constraintsList.length; i++){
+			if(index == i) tableau_var.push(1);
+			else tableau_var.push(0);
+		}
+		tableau_var.push(0);
+
+		for(var i = 0; i <= str.length; i++){
+			if(str[i] == '='){
+				tableau_var.push(parseFloat(str.substr(i+1, str.length)));
+				break;
+			}
+		}
+		
+		mat.push(tableau_var);
+	});
+
+	fxString = fxString.replace(/[^a-z0-9\+\-\.]/g, "");	// clean input function
+
+	tableau_var = [];
+
+	vars.forEach(function(data){
+		var pos = fxString.indexOf(data);
+		var coeff = 0;
+		
+		if(pos != -1){
+			for(var i = pos; i >= 0; i--){
+				if(fxString[i] == '+'){
+					coeff = parseFloat(fxString.substr(i+1, pos-1));
+					coeff = coeff ? coeff : 1;
+					break;
+	  			}
+	  			else if(fxString[i] == '-'){
+	  				coeff = 0 - parseFloat(fxString.substr(i+1, pos-1));
+	  				coeff = coeff ? coeff : -1;
+	  				break;
+	  			}
+	  			else if(i == 0){
+					coeff = parseFloat(fxString.substr(i, pos));
+					coeff = coeff ? coeff : 1;
+					break;
+	  			}
+			}
+		}
+			tableau_var.push(0 - coeff);
+	});
+		
+	for(var i = 0; i < constraintsList.length; i++){
+		tableau_var.push(0);
+	}
+	
+	tableau_var.push(1, 0);
+		
+	mat.push(tableau_var);
+	tableaus[0] = mat;
+	performOps();
+}
+
+function performOps(){
+	var tempMat;
+
+	while(1){
+	tempMat = (tableaus[tableaus.length - 1]).map(function(arr) {
+    	return arr.slice();
+	});
+
+	var temp = []
+	var TR = [];
+	var pivotCol_value = Math.min.apply(Math, tempMat[tempMat.length - 1]);
+	if(pivotCol_value >= 0) break;
+
+	var pivotCol_i = (tempMat[tempMat.length - 1]).indexOf(pivotCol_value);
+
+	for(var i = 1; i < tempMat.length - 1; i++){
+		TR[i-1] = tempMat[i][(tempMat[i]).length - 1] / tempMat[i][pivotCol_i];
+		if(TR[i-1] < 0) TR[i-1] = 9999;
+	}
+
+	var pivotRow_value = Math.min.apply(Math, TR);
+	var pivotRow_i = TR.indexOf(pivotRow_value) + 1;
+
+	var pivotValue = tempMat[pivotRow_i][pivotCol_i];
+
+	console.log(pivotRow_value + " " + pivotCol_value + " " + pivotValue);
+
+	var pivotRow = tempMat[pivotRow_i];
+	for(var i = 0; i < pivotRow.length; i++){
+		pivotRow[i] = pivotRow[i]/pivotValue;
+	}
+
+	for(var j = 1; j < tempMat.length; j++){
+		if(j!=pivotRow_i){
+			var t = tempMat[j][pivotCol_i];
+				for(var i = 0; i < pivotRow.length; i++){
+					temp[i] = pivotRow[i] * t;
+					tempMat[j][i] = tempMat[j][i] - temp[i];
+				}
+		}
+	}
+
+	tableaus.push(tempMat);
+	}
 }
